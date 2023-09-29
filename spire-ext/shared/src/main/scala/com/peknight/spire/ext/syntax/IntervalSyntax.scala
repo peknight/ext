2f4af29e9ed66ff89e6efcd8cbbef1ad765188f6
shuigen.rengang <rg249590@alibaba-inc.com>
2023-09-29 10:14:05 +0800

    update

--- spire-ext/shared/src/main/scala/com/peknight/spire/ext/syntax/IntervalSyntax.scala
+++ spire-ext/shared/src/main/scala/com/peknight/spire/ext/syntax/IntervalSyntax.scala
@@ -1,13 +1,20 @@
 package com.peknight.spire.ext.syntax
 
+import cats.kernel.Order
+import com.peknight.spire.ext.math.interval.IntervalOps
 import com.peknight.spire.ext.math.interval.time.LocalDateIntervalOps
 import spire.math.Interval
 
 import java.time.{LocalDate, Year}
 
 trait IntervalSyntax:
+  extension [N: Integral: Order] (interval: Interval[N])
+    def close: Interval[N] = IntervalOps.close(interval)
+  end extension
+
   extension (date: Interval[LocalDate])
     def mapToYear: Interval[Year] = LocalDateIntervalOps.mapToYear(date)
+    def close: Interval[LocalDate] = IntervalOps.close(date)
   end extension
 end IntervalSyntax
 object IntervalSyntax extends IntervalSyntax
