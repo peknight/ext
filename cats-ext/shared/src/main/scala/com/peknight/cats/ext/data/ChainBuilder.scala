5dc8a910d11ca806d33b8aa10a7f5853e2ab0332
peknight <JKpeknight@gmail.com>
2023-12-12 20:07:37 +0800

    add chain builder

---
+++ cats-ext/shared/src/main/scala/com/peknight/cats/ext/data/ChainBuilder.scala
@@ -0,0 +1,14 @@
+package com.peknight.cats.ext.data
+
+import cats.data.Chain
+
+import scala.collection.mutable
+
+class ChainBuilder[A] extends mutable.Builder[A, Chain[A]]:
+  private[this] var xs: Chain[A] = Chain.nil
+  final def clear(): Unit = xs = Chain.nil
+  final def result(): Chain[A] = xs
+  final def addOne(elem: A): this.type =
+    xs = xs.append(elem)
+    this
+end ChainBuilder
