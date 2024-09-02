b770760f6cbbf18e450a88633026373f86451209
peknight <JKpeknight@gmail.com>
2024-09-02 23:26:29 +0800

    update

--- scodec-bits-ext/shared/src/main/scala/com/peknight/scodec/bits/ext/package.scala
+++ scodec-bits-ext/shared/src/main/scala/com/peknight/scodec/bits/ext/package.scala
@@ -8,7 +8,5 @@ package object ext:
     else if bytes.length == length then bytes
     else bytes ++ ByteVector.fill(length - bytes.length)(0)
   def leftHalf(bytes: ByteVector): ByteVector = bytes.take(bytes.length / 2)
-  def rightHalf(bytes: ByteVector): ByteVector =
-    val half = bytes.length / 2
-    bytes.drop(half).take(half)
+  def rightHalf(bytes: ByteVector): ByteVector = bytes.drop(bytes.length / 2)
 end ext
