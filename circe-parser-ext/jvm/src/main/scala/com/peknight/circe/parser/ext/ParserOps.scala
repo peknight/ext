8f2c8d5beba6c919924c6decb814df4252857eb6
peknight <JKpeknight@gmail.com>
2024-09-17 22:36:42 +0800

    update

--- circe-parser-ext/jvm/src/main/scala/com/peknight/circe/parser/ext/ParserOps.scala
+++
@@ -1,8 +0,0 @@
-package com.peknight.circe.parser.ext
-
-import io.circe.{Decoder, Error, Json, ParsingFailure}
-
-object ParserOps:
-  def parse(input: String): Either[ParsingFailure, Json] = io.circe.parser.parse(input)
-  def decode[A: Decoder](input: String): Either[Error, A] = io.circe.parser.decode[A](input)
-end ParserOps
