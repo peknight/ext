642fe22f78336a437f03e1866845ae6fe4af1822
peknight <JKpeknight@gmail.com>
2025-02-18 15:41:36 +0800

    update

--- http4s-ext/shared/src/main/scala/com/peknight/http4s/ext/MediaRange.scala
+++ http4s-ext/shared/src/main/scala/com/peknight/http4s/ext/MediaRange.scala
@@ -4,4 +4,6 @@ import org.http4s.MediaType
 
 object MediaRange:
   val `application/json`: MediaType = MediaType.unsafeParse("application/json")
+  val `application/problem+json`: MediaType = MediaType.unsafeParse("application/problem+json")
+  val `application/problem+xml`: MediaType = MediaType.unsafeParse("application/problem+xml")
 end MediaRange
