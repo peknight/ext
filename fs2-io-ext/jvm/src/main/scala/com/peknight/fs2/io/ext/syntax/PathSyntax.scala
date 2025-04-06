f70a7e1c48edf8f497f40f79fbbd676fc903c42a
peknight <JKpeknight@gmail.com>
2025-04-06 19:01:21 +0800

    update

--- fs2-io-ext/jvm/src/main/scala/com/peknight/fs2/io/ext/syntax/PathSyntax.scala
+++ fs2-io-ext/jvm/src/main/scala/com/peknight/fs2/io/ext/syntax/PathSyntax.scala
@@ -7,9 +7,6 @@ import fs2.io.file.Path
 import java.nio.file.Paths
 
 trait PathSyntax:
-  extension (path: Path)
-    def exists[F[_]: Sync]: F[Boolean] = Sync[F].blocking(path.toNioPath.toFile.exists())
-  end extension
   extension (path: Path.type)
     def fromResource[F[_]: Sync](resourcePath: String): F[Path] =
       Sync[F].blocking(Path.fromNioPath(Paths.get(getClass.getClassLoader.getResource(resourcePath).toURI)))
