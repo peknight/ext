feedd867c6ff60505a2730260e56e60a93471b4f
peknight <JKpeknight@gmail.com>
2025-05-31 20:39:29 +0800

    update

--- http4s-client-ext/shared/src/main/scala/com/peknight/http4s/client/ext/proxy/ReverseConfig.scala
+++ http4s-client-ext/shared/src/main/scala/com/peknight/http4s/client/ext/proxy/ReverseConfig.scala
@@ -1,9 +1,15 @@
 package com.peknight.http4s.client.ext.proxy
 
 import com.comcast.ip4s.{Host, Port}
+import com.peknight.http4s.ext.syntax.uri.withAuthority
+import org.http4s.Uri
 
 trait ReverseConfig:
   def host: Host
   def port: Option[Port]
   def subdomain: String
+
+  def reverseUriMapper: PartialFunction[Uri, Uri] =
+    case uri if uri.host.exists(_.value.contains(subdomain)) =>
+      uri.withAuthority(org.http4s.Uri.Host.fromIp4sHost(host), port)
 end ReverseConfig
