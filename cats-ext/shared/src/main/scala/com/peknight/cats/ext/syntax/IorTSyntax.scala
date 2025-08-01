98e12e5c2d24a34058fb2d601daf0ec20df73cc2
peknight <JKpeknight@gmail.com>
2025-08-01 10:23:48 +0800

    update

--- cats-ext/shared/src/main/scala/com/peknight/cats/ext/syntax/IorTSyntax.scala
+++ cats-ext/shared/src/main/scala/com/peknight/cats/ext/syntax/IorTSyntax.scala
@@ -10,16 +10,16 @@ import cats.{Applicative, ApplicativeError, Functor, Monad, Semigroup}
 
 trait IorTSyntax:
   extension [A, B] (ior: Ior[A, B])
-    def eLiftIT[F[_]: Applicative]: IorT[F, A, B] = IorT(ior.pure[F])
+    def iLiftIT[F[_]: Applicative]: IorT[F, A, B] = IorT(ior.pure[F])
   end extension
   extension [A] (a: A)
-    def lLiftIT[F[_] : Applicative, R]: IorT[F, A, R] = a.leftIor[R].eLiftIT[F]
-    def rLiftIT[F[_]: Applicative, L]: IorT[F, L, A] = a.rightIor[L].eLiftIT[F]
+    def lLiftIT[F[_] : Applicative, R]: IorT[F, A, R] = a.leftIor[R].iLiftIT[F]
+    def rLiftIT[F[_]: Applicative, L]: IorT[F, L, A] = a.rightIor[L].iLiftIT[F]
   end extension
   extension [F[_], A] (fa: F[A])
     def flLiftIT[R](using Functor[F]): IorT[F, A, R] = IorT(fa.map(_.leftIor[R]))
     def frLiftIT[L](using Functor[F]): IorT[F, L, A] = IorT(fa.map(_.rightIor[L]))
-    def feLiftIT[E](using ApplicativeError[F, E]): IorT[F, E, A] = IorT(fa.attempt.map(_.toIor))
+    def fiLiftIT[E](using ApplicativeError[F, E]): IorT[F, E, A] = IorT(fa.attempt.map(_.toIor))
   end extension
   extension [F[_], A] (iorT: IorT[F, A, Boolean])
     def &&(that: => IorT[F, A, Boolean])(using Monad[F], Semigroup[A]): IorT[F, A, Boolean] = iorT.flatMap {
